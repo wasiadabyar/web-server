b3b8f644a2a4019a563bc83777ce80d53137d14f
wasiadabyar <wasiadabyar111@gmail.com>
2022-05-31 10:55:33 +0430

    setup for heroku

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const path = require('path');
 const hbs = require('hbs');
 
 const app = express();
-const publicPath = path.join(__dirname,'./public');
-const viewPath = path.join(__dirname,'./tamplates/views');
-const partialsPath = path.join(__dirname,'./tamplates/partials');
+const publicPath = path.join(__dirname, './public');
+const viewPath = path.join(__dirname, './tamplates/views');
+const partialsPath = path.join(__dirname, './tamplates/partials');
+
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 app.set('views', viewPath);
@@ -22,13 +24,13 @@ app.get('/home', (req, res) => {
     res.render('index')
 });
 app.get('/post', (req, res) => {
-    res.render('post',{
+    res.render('post', {
         'title': 'post',
         'body': 'body'
     })
 });
 
 
-app.listen(3000, () => {
-    console.log('App listening on pot 3000!');
+app.listen(port, () => {
+    console.log('App listening on port ' + port + '!');
 });
